e7c2ee48b9cf3c88301fc0ed2852530d64b58356
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-20 11:17:02 -0600

    ✅ Pass test: Report class save report

--- src/single_responsability_principle.py
+++ src/single_responsability_principle.py
@@ -1,5 +1,11 @@
+import pathlib
+
+
 class Report:
     def __init__(self, report_content: str):
         self.content = report_content
     def generate(self):
         return f"Report content: {self.content}"
+    def save_to_file(self, filename):
+        self.file = pathlib.Path(filename)
+        self.file.write_text(self.content)
