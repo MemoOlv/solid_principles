0c512cc7b7236d60fc3f61041b26a66d4ddfb4af
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-20 11:14:18 -0600

    🧪 Add failing test: Report saving

--- tests/test_single_responsability_principle.py
+++ tests/test_single_responsability_principle.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from src.single_responsability_principle import Report
 
 def test_report():
@@ -6,3 +8,12 @@ def test_report():
     obtained_report_generation = report.generate()
     expected_report_generation = "Report content: This is the content of the report"
     assert obtained_report_generation == expected_report_generation
+
+    output_file = "tests/report.txt"
+    file = pathlib.Path(output_file)
+
+    if file.exists():
+        file.unlink()
+
+    report.save_to_file(output_file)
+    assert file.exists()
