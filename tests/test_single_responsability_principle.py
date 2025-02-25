db0d34ba26c0038f23dc8706acd7f03aad9b2ef9
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-25 11:05:57 -0600

    ♻️ Refactor: Split tests for report generation and report saving

--- tests/test_single_responsability_principle.py
+++ tests/test_single_responsability_principle.py
@@ -1,21 +1,25 @@
 import pathlib
 
-from src.single_responsability_principle import Report
+from src.single_responsability_principle import Report, ReportSaver
+
+report_content = "This is the content of the report"
+report = Report(report_content)
 
 def test_report():
-    report_content = "This is the content of the report"
-    report = Report(report_content)
     obtained_report_generation = report.generate()
     expected_report_generation = "Report content: This is the content of the report"
     assert obtained_report_generation == expected_report_generation
 
+
+def test_report_saving():
     output_file = "tests/report.txt"
     file = pathlib.Path(output_file)
 
     if file.exists():
         file.unlink()
 
-    report.save_to_file(output_file)
+    report_saver = ReportSaver(report)
+    report_saver.save_to_file(output_file)
     assert file.exists()
 
     if file.exists():
