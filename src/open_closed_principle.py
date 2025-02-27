8a71873822203dc36557d297932e1a661f46a535
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-27 11:13:57 -0600

    ✅ Pass test: Add class Rectangle and function calculate_area

---
+++ src/open_closed_principle.py
@@ -0,0 +1,8 @@
+class Rectangle:
+    def __init__(self, width: float, height: float):
+        self.width: float = width
+        self.height: float = height
+
+def calculate_area(shape) -> float:
+    if isinstance(shape, Rectangle):
+        return shape.width * shape.height
