5ebfbb945cccfdbe99a9deba2d23421b676e9811
MemoOlv <guillermo.olvera@islas.org.mx>
2025-02-27 11:22:32 -0600

    🧪 Add failing test: Add class Circle

--- tests/test_open_closed_principle.py
+++ tests/test_open_closed_principle.py
@@ -1,7 +1,16 @@
-from src.open_closed_principle import Rectangle, calculate_area
+from src.open_closed_principle import Rectangle, calculate_area, Circle
+
+from pytest import approx
 
 def test_rectangle_area():
     rect = Rectangle(12, 8)
     obtained = calculate_area(rect)
     expected = 96
     assert obtained == expected
+
+def test_circle_area():
+    radius = 6.5
+    circle = Circle(radius)
+    obtained = calculate_area(circle)
+    expected = 6.
+    assert obtained == approx(expected)
