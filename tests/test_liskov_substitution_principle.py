bf9f0ae6114d836d4f667caa598c6ce4aa957ce2
MemoOlv <guillermo.olvera@islas.org.mx>
2025-03-04 11:19:26 -0600

    🚨🧪 Add faliling test: Violation of liskov principle🚨

--- tests/test_liskov_substitution_principle.py
+++ tests/test_liskov_substitution_principle.py
@@ -1,7 +1,12 @@
-from src.liskov_substitution_principle import Bird, make_bird_fly
+from src.liskov_substitution_principle import Bird, make_bird_fly, Penguin
 
 def test_flying_penguin():
     bird = Bird()
     obtained = make_bird_fly(bird)
     expected = "I can fly"
+    assert obtained == expected
+
+    penguin = Penguin()
+    obtained = make_bird_fly(penguin)
+    expected = "I can't fly"
     assert obtained == expected
