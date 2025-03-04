3809461c1134c65d82f0ffdf37308c488b98d6e4
MemoOlv <guillermo.olvera@islas.org.mx>
2025-03-04 10:43:13 -0600

    ✅ Pass test: Add class Circle

--- src/open_closed_principle.py
+++ src/open_closed_principle.py
@@ -1,4 +1,5 @@
 from typing import Protocol
+from math import pi
 
 class Shape(Protocol):
     def area(self) -> float:
@@ -11,6 +12,12 @@ class Rectangle:
         self.height: float = height
     def area(self) -> float:
         return self.width * self.height
+
+class Circle:
+    def __init__(self, radius: float):
+        self.radius: float = radius
+    def area(self) -> float:
+        return pi * (self.radius ** 2)
 
 def calculate_area(shape: Shape) -> float:
     return shape.area()
