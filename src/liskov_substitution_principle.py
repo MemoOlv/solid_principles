b1fe356a763de5f7912d1316fe33d78f37d7f062
MemoOlv <guillermo.olvera@islas.org.mx>
2025-03-04 11:20:38 -0600

    🚨✅ Pass test: Add class Penguin. Violation of liskov substitution principle 🚨

--- src/liskov_substitution_principle.py
+++ src/liskov_substitution_principle.py
@@ -2,5 +2,9 @@ class Bird:
     def fly(self):
         return "I can fly"
 
+class Penguin(Bird):
+    def fly(self):
+        return "I can't fly"
+
 def make_bird_fly(bird):
     return bird.fly()
