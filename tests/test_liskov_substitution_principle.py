47c0509e00d330b5185447a90a7944367b3bfa42
MemoOlv <guillermo.olvera@islas.org.mx>
2025-03-05 10:44:18 -0600

    🧪 Add failing test: Introduce make_bird_move()

--- tests/test_liskov_substitution_principle.py
+++ tests/test_liskov_substitution_principle.py
@@ -1,10 +1,15 @@
-from src.liskov_substitution_principle import Bird, make_bird_fly, Penguin
+from src.liskov_substitution_principle import Bird, make_bird_fly, Penguin, make_bird_move
 
 def test_flying_penguin():
     bird = Bird()
     obtained = make_bird_fly(bird)
     expected = "I can fly"
     assert obtained == expected
+
+    obtained = make_bird_move(bird)
+    expected = "I am moving"
+    assert obtained == expected
+
 
     penguin = Penguin()
     obtained = make_bird_fly(penguin)
