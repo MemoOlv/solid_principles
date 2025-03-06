0e2a4c446d8326e8f676c7dd6b22035f783b3aea
MemoOlv <guillermo.olvera@islas.org.mx>
2025-03-06 11:15:48 -0600

    ✅ Pass test: Add segregation for scanning

--- src/interface_segregation_principle.py
+++ src/interface_segregation_principle.py
@@ -4,9 +4,14 @@ class PrinterProtocol(Protocol):
     def print_document(self):
         pass
 
+class ScannerProtocol(Protocol):
+    def scan_document(self):
+        pass
+
 class AllInOnePrinter:
     def __init__(self):
         self.printer = Printer()
+        self.scanner = Scanner()
 
     def print_document(self):
         return do_the_print(self.printer)
@@ -19,5 +24,12 @@ class Printer(PrinterProtocol):
     def print_document(self):
         return "Printing"
 
+class Scanner(PrinterProtocol):
+    def scan_document(self):
+        return "Scanning"
+
 def do_the_print(printer: Printer):
     return printer.print_document()
+
+def do_the_scan(scanner: Scanner):
+    return scanner.scan_document()
