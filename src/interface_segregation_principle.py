fbf27abbb5d6be5a4697559737399574ab9e5ad4
MemoOlv <guillermo.olvera@islas.org.mx>
2025-03-06 10:55:26 -0600

    ✅🚨 Pass test: Add class AllInOnePrinter. Violation of interface segregation principle🚨

---
+++ src/interface_segregation_principle.py
@@ -0,0 +1,7 @@
+class AllInOnePrinter:
+    def print_document(self):
+        return "Printing"
+    def scan_document(self):
+        return "Scanning"
+    def fax_document(self):
+        return "Faxing"
