29f11547da1ef9c8f9249fc81bf4d72b183588d5
MemoOlv <guillermo.olvera@islas.org.mx>
2025-03-13 09:39:27 -0600

    ✅ Pass test: Add Faxer class and do_the_fax() function

--- src/interface_segregation_principle.py
+++ src/interface_segregation_principle.py
@@ -1,12 +1,19 @@
 from typing import Protocol
 
+
 class PrinterProtocol(Protocol):
     def print_document(self):
         pass
 
+
 class ScannerProtocol(Protocol):
     def scan_document(self):
         pass
+
+class FaxerProtocol(Protocol):
+    def fax_document(self):
+        pass
+
 
 class AllInOnePrinter:
     def __init__(self):
@@ -15,21 +22,35 @@ class AllInOnePrinter:
 
     def print_document(self):
         return do_the_print(self.printer)
+
     def scan_document(self):
         return "Scanning"
+
     def fax_document(self):
         return "Faxing"
+
 
 class Printer(PrinterProtocol):
     def print_document(self):
         return "Printing"
 
+
 class Scaner(PrinterProtocol):
     def scan_document(self):
         return "Scanning"
 
+
+class Faxer(PrinterProtocol):
+    def fax_document(self):
+        return "Faxing"
+
+
 def do_the_print(printer: Printer):
     return printer.print_document()
 
+
 def do_the_scan(scanner: Scaner):
     return scanner.scan_document()
+
+def do_the_fax(faxer: Faxer):
+    return faxer.fax_document()
